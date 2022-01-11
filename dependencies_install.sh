984cf114683f3867c363660cfd43c4daba6a3e61
futurevoid <72345439+futurevoid@users.noreply.github.com>
2022-01-11 15:51:09 +0300

    Add files via upload

---
+++ dependencies_install.sh
@@ -0,0 +1,5 @@
+python3 -m pip install pip wheel setuptools
+
+python3 -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew --extra-index-url https://kivy.org/downloads/packages/simple/
+
+python3 -m pip install kivy qrcode
