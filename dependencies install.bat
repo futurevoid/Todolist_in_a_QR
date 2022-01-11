abbc97faba83e4234764506e08e5928888d951ee
futurevoid <72345439+futurevoid@users.noreply.github.com>
2022-01-11 15:51:34 +0300

    Update and rename kivy install.bat to dependencies install.bat

--- dependencies install.bat
+++ dependencies install.bat
@@ -2,4 +2,4 @@ python -m pip install --upgrade pip wheel setuptools
 
 python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew --extra-index-url https://kivy.org/downloads/packages/simple/
 
-python -m pip install kivy
+python -m pip install kivy qrcode
